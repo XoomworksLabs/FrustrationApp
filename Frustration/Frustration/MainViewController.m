e9a1dca043b1c9c4b96c48fcb4d09663d737a6bc
Mihail Popa <mihail.popa@xoomworks.com>
2012-12-11 11:04:15 +0200

    Rotation set to NO for the main view controller also

--- Frustration/Frustration/MainViewController.m
+++ Frustration/Frustration/MainViewController.m
@@ -71,8 +71,7 @@
 
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
 {
-    // Return YES for supported orientations
-	return YES;
+	return NO;
 }
 -(void)performSegueWithIdentifier:(NSString *)identifier sender:(id)sender
 {
